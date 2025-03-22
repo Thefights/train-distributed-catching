d0b0c765cc00310ae1fa95c725ce075312db4c7e
QuachKhang <phuckhang1088@gmail.com>
2025-03-22 11:37:14 +0700

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace train_distributed_catching.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
